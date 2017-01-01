d4b3c32659cec5d335851ed8c24b054a2eb6de8e
Prasanna Venkat <to.srini@gmail.com>
2017-01-01 15:23:47 -0800

    First step in making the rows of a table clickable. Need to use Angular directives to make it work

---
+++ src/main/webapp/app.js
@@ -0,0 +1,18 @@
+
+// Make all table's rows as clickable if it has data-href set.
+// http://stackoverflow.com/a/29129307/7247103
+// TODO: Make it run on a change to any of the tables we render. Looks like Angular directives can help here.
+$(function() {
+    $('.table tr[data-href]').each(function() {
+        $(this).css('cursor','pointer').hover(
+            function() {
+                $(this).addClass('active');
+            },
+            function() {
+                $(this).removeClass('active');
+            }).click( function() {
+                document.location = $(this).attr('data-href');
+            }
+        );
+    });
+});
