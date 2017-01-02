9d43b96bfd3bb982d90c239f26acf11117105884
Prasanna Venkat <to.srini@gmail.com>
2017-01-02 13:40:41 -0800

    Make the trip list table sortable

--- src/main/webapp/trip-list/trip-list.component.js
+++ src/main/webapp/trip-list/trip-list.component.js
@@ -7,9 +7,24 @@ angular.
     controller: ['$http', function TripListController($http) {
       var self = this;
 
+      self.orderByField = 'departureDateAndTime';
+      self.reverseOrder = true;
+
+      self.orderBy = function (field) {
+        self.reverseOrder = (field === self.orderByField) ? !self.reverseOrder : false;
+        self.orderByField = field;
+      };
+
       $http.get('/api/v1/trips').then(function (response) {
         console.log("response", response);
         self.trips = response.data;
+
+        // Convert the date in string format to JavaScript Date objects.
+        for (var i = 0, len = self.trips.length; i < len; ++i) {
+          var trip = self.trips[i];
+          trip.departureDateAndTime = new Date(trip.departureDateAndTime);
+          trip.createdOn = new Date(trip.createdOn);
+        }
       });
     }
     ]
